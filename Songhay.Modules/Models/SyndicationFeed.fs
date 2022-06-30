80dccaba826c850cd0d83ad6af15032a1b36df29
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-06-29 17:26:46 -0700

    🚜✨

---
+++ Songhay.Modules/Models/SyndicationFeed.fs
@@ -0,0 +1,19 @@
+﻿namespace Songhay.Modules.Models
+
+open System
+
+type SyndicationFeedItem =
+    {
+        title: string
+        link: string
+        extract: string option
+        publicationDate: DateTime option
+    }
+
+type SyndicationFeed =
+    {
+        feedImage: string option
+        feedItems: SyndicationFeedItem list
+        feedTitle: string
+        modificationDate: DateTime
+    }
