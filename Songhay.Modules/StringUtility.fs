024f84235372993aa84b360cd9518758619e61a3
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-08-14 19:09:13 -0700

    resolves #2 🔨

--- Songhay.Modules/StringUtility.fs
+++ Songhay.Modules/StringUtility.fs
@@ -14,18 +14,35 @@ module StringUtility =
         Regex.Replace(input, pattern, replacement, options)
 
     /// <summary>
-    /// Tries to call <see cref="Regex.Replace"/>
-    /// for a successful find-change operation.
+    /// Demarcates the <see cref="string"/>
+    /// by the specified demarcation <see cref="char"/>
+    /// or returns <see cref="None" />.
     /// </summary>
-    /// <param name="pattern">The regular expression pattern to match.</param>
-    /// <param name="replacement">The replacement string.</param>
-    /// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
-    /// <param name="input">The string to search for a match.</param>
-    let tryRegexReplace (pattern: string) (replacement: string) (options: RegexOptions) (input: string) =
-        try
-            Ok (input |> regexReplace pattern replacement options)
-        with
-        | ex -> Error ex
+    /// <param name="demarcation">The demarcation <see cref="char"/>.</param>
+    /// <param name="input">The input.</param>
+    /// <remarks>
+    /// See <see cref="toKabobCase" /> and <see cref="toSnakeCase" />
+    /// for specific applications of this generalized function.
+    /// </remarks>
+    let demarcateByCase (demarcation: char) (input: string) =
+        if String.IsNullOrWhiteSpace input then None
+        else
+            let processChar i c =
+                if (i > 0) && Char.IsUpper(c) then $"{demarcation}{Char.ToLower(c)}"
+                else $"{Char.ToLower(c)}"
+            let stringArray = input.ToCharArray() |> Array.mapi processChar
+            Some (String.Join(String.Empty, stringArray))
+
+    /// <summary>
+    /// Calls <see cref="String.ToLowerInvariant"/>
+    /// on the first character of the specified <see cref="string" />.
+    /// </summary>
+    /// <remarks>
+    /// The member is here because Microsoft’s <c>JsonElement.GetProperty</c> method
+    /// matches property names with “ordinal, case-sensitive” comparisons.
+    /// [ https://docs.microsoft.com/en-us/dotnet/api/system.text.json.jsonelement.getproperty?view=net-6.0 ]
+    /// </remarks>
+    let toCamelCase (input: string) = $"{input[0].ToString().ToLowerInvariant()}{input[1..]}"
 
     /// <summary>
     /// Calls <see cref="String.ToLowerInvariant"/>
@@ -75,23 +92,11 @@ module StringUtility =
 
     /// <summary>
     /// Demarcates the <see cref="string"/>
-    /// by the specified demarcation <see cref="char"/>
+    /// by the demarcation <see cref="char"/>, <c>'-'</c>,
     /// or returns <see cref="None" />.
     /// </summary>
-    /// <param name="demarcation">The demarcation <see cref="char"/>.</param>
     /// <param name="input">The input.</param>
-    /// <remarks>
-    /// See <see cref="toKabobCase" /> and <see cref="toSnakeCase" />
-    /// for specific applications of this generalized function.
-    /// </remarks>
-    let demarcateByCase (demarcation: char) (input: string) =
-        if String.IsNullOrWhiteSpace input then None
-        else
-            let processChar i c =
-                if (i > 0) && Char.IsUpper(c) then $"{demarcation}{Char.ToLower(c)}"
-                else $"{Char.ToLower(c)}"
-            let stringArray = input.ToCharArray() |> Array.mapi processChar
-            Some (String.Join(String.Empty, stringArray))
+    let toKabobCase (input: string) = input |> demarcateByCase '-'
 
     /// <summary>
     /// Converts the <see cref="string"/> into a numeric format for parsing
@@ -108,16 +113,22 @@ module StringUtility =
 
     /// <summary>
     /// Demarcates the <see cref="string"/>
-    /// by the demarcation <see cref="char"/>, <c>'-'</c>,
-    /// or returns <see cref="None" />.
-    /// </summary>
-    /// <param name="input">The input.</param>
-    let toKabobCase (input: string) = input |> demarcateByCase '-'
-
-    /// <summary>
-    /// Demarcates the <see cref="string"/>
     /// by the demarcation <see cref="char"/>, <c>'_'</c>,
     /// or returns <see cref="None" />.
     /// </summary>
     /// <param name="input">The input.</param>
     let toSnakeCase (input: string) = input |> demarcateByCase '_'
+
+    /// <summary>
+    /// Tries to call <see cref="Regex.Replace"/>
+    /// for a successful find-change operation.
+    /// </summary>
+    /// <param name="pattern">The regular expression pattern to match.</param>
+    /// <param name="replacement">The replacement string.</param>
+    /// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
+    /// <param name="input">The string to search for a match.</param>
+    let tryRegexReplace (pattern: string) (replacement: string) (options: RegexOptions) (input: string) =
+        try
+            Ok (input |> regexReplace pattern replacement options)
+        with
+        | ex -> Error ex
